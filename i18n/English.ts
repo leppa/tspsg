721c99d48236b5bc2472843c8b4ba373b580a7b7
Oleksii Serdiuk <contacts@oleksii.name>
2007-10-15 18:36:43 +0000

    Translation files update to reflect updates and changes in interface...

--- i18n/English.ts
+++ i18n/English.ts
@@ -3,226 +3,371 @@
 <context encoding="UTF-8">
     <name>MainWindow</name>
     <message encoding="UTF-8">
-        <location filename="../resources/mainwindow.ui" line="19"/>
+        <location filename="../ui/mainwindow.ui" line="13"/>
         <source>Задача коммивояжера</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../resources/mainwindow.ui" line="39"/>
-        <source>Результат</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message encoding="UTF-8">
-        <location filename="../resources/mainwindow.ui" line="69"/>
-        <source>Задание</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message encoding="UTF-8">
-        <location filename="../resources/mainwindow.ui" line="90"/>
+        <location filename="../ui/mainwindow.ui" line="54"/>
         <source>&amp;Вариант:</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../resources/mainwindow.ui" line="110"/>
+        <location filename="../ui/mainwindow.ui" line="77"/>
         <source>&amp;Городов:</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../resources/mainwindow.ui" line="166"/>
+        <location filename="../ui/mainwindow.ui" line="165"/>
         <source>Заполнить таблицу случайными значениями</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../resources/mainwindow.ui" line="169"/>
+        <location filename="../ui/mainwindow.ui" line="168"/>
         <source>Случайно</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../resources/mainwindow.ui" line="179"/>
+        <location filename="../ui/mainwindow.ui" line="178"/>
         <source>Решить текущую задачу</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../resources/mainwindow.ui" line="182"/>
+        <location filename="../ui/mainwindow.ui" line="181"/>
         <source>Решить</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../resources/mainwindow.ui" line="208"/>
+        <location filename="../ui/mainwindow.ui" line="247"/>
         <source>&amp;Файл</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../resources/mainwindow.ui" line="212"/>
+        <location filename="../ui/mainwindow.ui" line="254"/>
         <source>Со&amp;хранить</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../resources/mainwindow.ui" line="232"/>
+        <location filename="../ui/mainwindow.ui" line="276"/>
         <source>&amp;Настройки</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../resources/mainwindow.ui" line="238"/>
+        <location filename="../ui/mainwindow.ui" line="282"/>
         <source>&amp;Помощь</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../resources/mainwindow.ui" line="281"/>
+        <location filename="../ui/mainwindow.ui" line="324"/>
         <source>&amp;Настройка печати</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../resources/mainwindow.ui" line="289"/>
+        <location filename="../ui/mainwindow.ui" line="338"/>
         <source>&amp;Печать...</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../resources/mainwindow.ui" line="297"/>
+        <location filename="../ui/mainwindow.ui" line="352"/>
         <source>&amp;Ход решения</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../resources/mainwindow.ui" line="302"/>
+        <location filename="../ui/mainwindow.ui" line="363"/>
         <source>&amp;Граф решения</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../resources/mainwindow.ui" line="321"/>
+        <location filename="../ui/mainwindow.ui" line="388"/>
         <source>&amp;Создать</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../resources/mainwindow.ui" line="332"/>
+        <location filename="../ui/mainwindow.ui" line="402"/>
         <source>&amp;Открыть</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../resources/mainwindow.ui" line="343"/>
+        <location filename="../ui/mainwindow.ui" line="419"/>
         <source>&amp;Параметры</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../resources/mainwindow.ui" line="351"/>
+        <location filename="../ui/mainwindow.ui" line="430"/>
         <source>&amp;Содежание</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../resources/mainwindow.ui" line="359"/>
+        <location filename="../ui/mainwindow.ui" line="441"/>
         <source>&amp;Контекстная справка</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../resources/mainwindow.ui" line="367"/>
+        <location filename="../ui/mainwindow.ui" line="452"/>
         <source>&amp;Про...</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../resources/mainwindow.ui" line="375"/>
+        <location filename="../ui/mainwindow.ui" line="460"/>
         <source>В&amp;ыход</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../resources/mainwindow.ui" line="378"/>
+        <location filename="../ui/mainwindow.ui" line="463"/>
         <source>Выйти из программы</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../resources/mainwindow.ui" line="292"/>
+        <location filename="../ui/mainwindow.ui" line="344"/>
         <source>Ctrl+P</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../resources/mainwindow.ui" line="313"/>
+        <location filename="../ui/mainwindow.ui" line="483"/>
         <source>Ctrl+S</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../resources/mainwindow.ui" line="324"/>
+        <location filename="../ui/mainwindow.ui" line="394"/>
         <source>Ctrl+N</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../resources/mainwindow.ui" line="335"/>
+        <location filename="../ui/mainwindow.ui" line="411"/>
         <source>Ctrl+O</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../resources/mainwindow.ui" line="310"/>
-        <source>Сохранить &amp;все</source>
+        <location filename="../src/mainwindow.cpp" line="83"/>
+        <source>Город </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../ui/mainwindow.ui" line="33"/>
+        <source>Задача</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../ui/mainwindow.ui" line="67"/>
+        <source>Номер варианта</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../ui/mainwindow.ui" line="90"/>
+        <source>Количество городов, которые обходит окммивояжер</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../ui/mainwindow.ui" line="121"/>
+        <source>Стоимости переходов/переездов из города в город</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../ui/mainwindow.ui" line="194"/>
+        <source>Решение</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../ui/mainwindow.ui" line="211"/>
+        <source>Ход решения</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../ui/mainwindow.ui" line="221"/>
+        <source>Граф решения</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../ui/mainwindow.ui" line="327"/>
+        <source>Настроить принтер для печати</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../ui/mainwindow.ui" line="341"/>
+        <source>Распечатать результаты решения</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../ui/mainwindow.ui" line="355"/>
+        <source>Сохранить только хоод решения</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../ui/mainwindow.ui" line="366"/>
+        <source>Сохранить только граф решения</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../ui/mainwindow.ui" line="374"/>
+        <source>&amp;Решение</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../ui/mainwindow.ui" line="377"/>
+        <source>Сохранить ход и граф решения</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../ui/mainwindow.ui" line="391"/>
+        <source>Создать новую задачу</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../ui/mainwindow.ui" line="405"/>
+        <source>Открыть задачу</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../ui/mainwindow.ui" line="408"/>
+        <source>Открыть сохраненное условие задачи</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../ui/mainwindow.ui" line="422"/>
+        <source>Настройка параметров программмы</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../ui/mainwindow.ui" line="433"/>
+        <source>Вызвать справку по программе</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../ui/mainwindow.ui" line="444"/>
+        <source>Вызвать контекстную справку</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../ui/mainwindow.ui" line="471"/>
+        <source>&amp;Задачу</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../ui/mainwindow.ui" line="474"/>
+        <source>Сохранить</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../ui/mainwindow.ui" line="477"/>
+        <source>Сохранить задачу</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../ui/mainwindow.ui" line="480"/>
+        <source>Сохранить условие задачи в файл</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>SettingsDialog</name>
     <message encoding="UTF-8">
-        <location filename="../resources/settingsdialog.ui" line="19"/>
+        <location filename="../ui/settingsdialog.ui" line="13"/>
         <source>Параметры</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../resources/settingsdialog.ui" line="81"/>
+        <location filename="../ui/settingsdialog.ui" line="48"/>
         <source>background-color: #0080C0</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../resources/settingsdialog.ui" line="191"/>
+        <location filename="../ui/settingsdialog.ui" line="158"/>
         <source>Случайные значения:</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../resources/settingsdialog.ui" line="199"/>
+        <location filename="../ui/settingsdialog.ui" line="172"/>
         <source>Ми&amp;нимум:</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../resources/settingsdialog.ui" line="226"/>
+        <location filename="../ui/settingsdialog.ui" line="208"/>
         <source>&amp;Максимум:</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../resources/settingsdialog.ui" line="257"/>
+        <location filename="../ui/settingsdialog.ui" line="242"/>
         <source>Настройки печати:</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../resources/settingsdialog.ui" line="265"/>
+        <location filename="../ui/settingsdialog.ui" line="250"/>
         <source>&amp;Левый отступ:</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../resources/settingsdialog.ui" line="296"/>
+        <location filename="../ui/settingsdialog.ui" line="287"/>
         <source>&amp;Шрифт...</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../resources/settingsdialog.ui" line="314"/>
+        <location filename="../ui/settingsdialog.ui" line="308"/>
         <source>Сохранять состояние и положение главного окна</source>
         <translation type="unfinished"></translation>
     </message>
-    <message encoding="UTF-8">
-        <location filename="../resources/settingsdialog.ui" line="384"/>
-        <source>Подсказка</source>
-        <translation type="unfinished"></translation>
-    </message>
     <message>
-        <location filename="../resources/settingsdialog.ui" line="403"/>
+        <location filename="../ui/settingsdialog.ui" line="412"/>
         <source>&amp;OK</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../resources/settingsdialog.ui" line="416"/>
+        <location filename="../ui/settingsdialog.ui" line="431"/>
         <source>&amp;Отмена</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../resources/settingsdialog.ui" line="365"/>
+        <location filename="../ui/settingsdialog.ui" line="365"/>
         <source>Помощь</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../resources/settingsdialog.ui" line="362"/>
+        <location filename="../ui/settingsdialog.ui" line="362"/>
         <source>Получить помощь по настройкам</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../ui/settingsdialog.ui" line="185"/>
+        <source>Нижняя граница для генерации случайных значений</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../ui/settingsdialog.ui" line="221"/>
+        <source>Верхняя граница для генерации случайных значений</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../ui/settingsdialog.ui" line="263"/>
+        <source>Размер отступа от левого края страницы</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../ui/settingsdialog.ui" line="284"/>
+        <source>Задать параметры шрифта, используемого при печати</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../ui/settingsdialog.ui" line="305"/>
+        <source>Открывать окно в том же положении и с теми же размерами, с какими оно было закрыто</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../ui/settingsdialog.ui" line="359"/>
+        <source>Справка</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../ui/settingsdialog.ui" line="390"/>
+        <source>Наведите указателем мыши на требуемый элемент диалога, чтоб получить подсказку</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../ui/settingsdialog.ui" line="409"/>
+        <source>Сохранить новые параметры</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../ui/settingsdialog.ui" line="428"/>
+        <source>Закрыть окно без сохранения параметров</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
