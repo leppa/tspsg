fb0c1d86f41602644dd2ce0b842b016ec2032a83
Oleksii Serdiuk <contacts@oleksii.name>
2013-09-03 23:19:43 +0200

    Added copyright boilerplates to BB10ProxyStyle files.

--- src/bb10proxystyle.cpp
+++ src/bb10proxystyle.cpp
@@ -1,3 +1,26 @@
+/*
+ *  TSPSG: TSP Solver and Generator
+ *  Copyright (C) 2007-2013 Oleksii Serdiuk <contacts[at]oleksii[dot]name>
+ *
+ *  $Id: $Format:%h %ai %an$ $
+ *  $URL: http://tspsg.info/ $
+ *
+ *  This file is part of TSPSG.
+ *
+ *  TSPSG is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  TSPSG is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with TSPSG.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "bb10proxystyle.h"
 
 BB10ProxyStyle::BB10ProxyStyle(QStyle *style)
--- src/bb10proxystyle.h
+++ src/bb10proxystyle.h
@@ -1,3 +1,30 @@
+/*!
+ * \file bb10proxystyle.h
+ * \author Copyright &copy; 2007-2013 Oleksii Serdiuk <contacts[at]oleksii[dot]name>
+ *
+ *  $Id: $Format:%h %ai %an$ $
+ *  $URL: http://tspsg.info/ $
+ *
+ * \brief Defines BB10ProxyStyle class.
+ *
+ *  <b>TSPSG: TSP Solver and Generator</b>
+ *
+ *  This file is part of TSPSG.
+ *
+ *  TSPSG is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  TSPSG is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with TSPSG.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef BB10PROXYSTYLE_H
 #define BB10PROXYSTYLE_H
 
